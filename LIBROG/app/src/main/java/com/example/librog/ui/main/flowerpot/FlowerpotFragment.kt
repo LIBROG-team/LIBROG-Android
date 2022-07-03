ae61f077f212d31f28dd98f42987b56e17291c3e
Jangwoo <wkdxod321@gmail.com>
2022-07-03 21:52:26 +0900

    Feat: add FlowerpotFragment, Fix: bottomNavigationView - 화분관리 탭 디자인 작업 완료, 더미데이터 삽입 - bottomNavigationView 중앙에 기록추가 버튼 생성   - FloatingActionButton으로 진행하려 했으나 오류 발생하여 수정 - bottomNavigationView 색상 변경 - FlowerpotData 데이터 클래스 생성 - RecyclerView 동작 확인

--- LIBROG/app/src/main/java/com/example/librog/ui/main/flowerpot/FlowerpotFragment.kt
+++ LIBROG/app/src/main/java/com/example/librog/ui/main/flowerpot/FlowerpotFragment.kt
@@ -1,13 +1,39 @@
 package com.example.librog.ui.main.flowerpot
 
+import androidx.recyclerview.widget.LinearLayoutManager
+import com.example.librog.R
+import com.example.librog.data.FlowerpotData
 import com.example.librog.databinding.FragmentFlowerpotBinding
 import com.example.librog.ui.BaseFragment
 
 class FlowerpotFragment : BaseFragment<FragmentFlowerpotBinding>(FragmentFlowerpotBinding::inflate) {
 
+    private var flowerpotDataList = ArrayList<FlowerpotData>()
 
     override fun initAfterBinding() {
+        initFlowerpotData()
 
+        val adapter = FlowerpotRVAdapter(flowerpotDataList)
+        binding.flowerpotListRv.adapter = adapter
+        binding.flowerpotListRv.layoutManager = LinearLayoutManager(context, LinearLayoutManager.VERTICAL, false)
+        binding.flowerpotTotalTv.text = String.format(getString(R.string.flowerpot_total),flowerpotDataList.size)
+        adapter.setMyItemClickListener(object : FlowerpotRVAdapter.OnItemClickListener{
+            override fun onItemClick(flowerpotData: FlowerpotData) {
+
+            }
+
+        })
+
+    }
+
+    private fun initFlowerpotData() {
+        val testList = arrayListOf<FlowerpotData>(
+            FlowerpotData("라넌큘러스", "2022.05.07", "2022.05.18", 3, "고전"),
+            FlowerpotData("튤립", "2022.03.11", "2022.05.07", 11, "미스터리"),
+            FlowerpotData("해바라기", "2022.2.21", "2022.03.11", 4, "SF"),
+            FlowerpotData("무궁화", "2022.05.07", "2022.05.18", 3, "고전")
+        )
+        flowerpotDataList.addAll(testList)
     }
 
 }
