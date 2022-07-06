6e026aef64aa133f8833055c5b7b18eb53b6ad5f
Jangwoo <wkdxod321@gmail.com>
2022-07-06 17:12:03 +0900

    Feat: Add AddBookFragment 기록추가 화면 추가, 데이터는 아직 넣지 않음

---
+++ LIBROG/app/src/main/java/com/example/librog/ui/main/addbook/AddBookFragment.kt
@@ -0,0 +1,12 @@
+package com.example.librog.ui.main.addbook
+
+import com.example.librog.databinding.FragmentAddBookBinding
+import com.example.librog.ui.BaseFragment
+
+class AddBookFragment : BaseFragment<FragmentAddBookBinding>(FragmentAddBookBinding::inflate) {
+    override fun initAfterBinding() {
+
+    }
+
+
+}
