03b884aa7bd45111ed02f697339b92355aa6005c
hyun2371 <loveanna1228@gmail.com>
2022-07-12 14:51:14 +0900

    Fix: addbookselectfragment edittext scrollbar

--- LIBROG/app/src/main/java/com/example/librog/ui/main/addbook/AddBookSelectActivity.kt
+++ LIBROG/app/src/main/java/com/example/librog/ui/main/addbook/AddBookSelectActivity.kt
@@ -1,7 +1,13 @@
 package com.example.librog.ui.main.addbook
 
+import android.content.Context
 import android.content.Intent
 import android.os.Bundle
+import android.text.method.ScrollingMovementMethod
+import android.view.MotionEvent
+import android.view.View
+import android.view.inputmethod.InputMethodManager
+import android.widget.EditText
 import androidx.appcompat.app.AppCompatActivity
 import com.example.librog.R
 import com.example.librog.databinding.ActivityAddBookSelectBinding
@@ -12,14 +18,15 @@ class AddBookSelectActivity : AppCompatActivity(){
 
     lateinit var binding: ActivityAddBookSelectBinding
 
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
         binding = ActivityAddBookSelectBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
-       initClickListener()
-
+        binding.addBookSelectReviewWriteEt.enableScrollText();
+        initClickListener()
 
     }
 
@@ -32,6 +39,24 @@ class AddBookSelectActivity : AppCompatActivity(){
         binding.addBookSelectBackBtn.setOnClickListener {
             finish()
 
+        }
+    }
+
+    fun EditText.enableScrollText()
+    {
+        overScrollMode = View.OVER_SCROLL_ALWAYS
+        scrollBarStyle = View.SCROLLBARS_INSIDE_INSET
+        isVerticalScrollBarEnabled = true
+        setOnTouchListener { view, event ->
+            if (view is EditText) {
+                if(!view.text.isNullOrEmpty()) {
+                    view.parent.requestDisallowInterceptTouchEvent(true)
+                    when (event.action and MotionEvent.ACTION_MASK) {
+                        MotionEvent.ACTION_UP -> view.parent.requestDisallowInterceptTouchEvent(false)
+                    }
+                }
+            }
+            false
         }
     }
 }
