b57baedbbe0978b561cf770fbc89f6eb4f25bf17
hyun2371 <loveanna1228@gmail.com>
2022-07-18 16:07:44 +0900

    Feat: Add history fragment RV

--- LIBROG/app/src/main/java/com/example/librog/ui/main/history/HistoryFragment.kt
+++ LIBROG/app/src/main/java/com/example/librog/ui/main/history/HistoryFragment.kt
@@ -5,14 +5,57 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Toast
+import androidx.recyclerview.widget.GridLayoutManager
+import androidx.recyclerview.widget.LinearLayoutManager
 import com.example.librog.R
+import com.example.librog.data.HistoryBookData
 import com.example.librog.databinding.FragmentHistoryBinding
-import com.example.librog.ui.BaseFragment
 
-class HistoryFragment : BaseFragment<FragmentHistoryBinding>(FragmentHistoryBinding::inflate) {
-    override fun initAfterBinding() {
+class HistoryFragment : Fragment() {
+    lateinit var binding: FragmentHistoryBinding
+    private var historybookDatas = ArrayList<HistoryBookData>()
 
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        binding = FragmentHistoryBinding.inflate(inflater, container, false)
+
+        initLayout()
+        initData()
+
+        return binding.root
     }
 
+    private fun initLayout(){
+        val historyRVAdapter = HistoryRVAdapter(historybookDatas)
+        //리사이클러뷰에 어댑터 연결
+        binding.historyBookListRv.adapter = historyRVAdapter
+        binding.historyBookListRv.layoutManager = LinearLayoutManager(context,
+            LinearLayoutManager.HORIZONTAL,false)
 
+
+
+        historyRVAdapter.setMyItemClickListener(object : HistoryRVAdapter.OnItemClickListener {
+            override fun onItemClick(tempHistoryBookData: HistoryBookData) {
+                Toast.makeText(activity,"Book Clicked",Toast.LENGTH_SHORT).show();
+            }
+        })
+
+        binding.historyBookListRv.layoutManager = GridLayoutManager(context,3)
+    }
+
+    private fun initData(){
+        historybookDatas.apply{
+            add(HistoryBookData(R.drawable.home_item_book1))
+            add(HistoryBookData(R.drawable.home_item_book2))
+            add(HistoryBookData(R.drawable.home_item_book1))
+            add(HistoryBookData(R.drawable.home_item_book2))
+            add(HistoryBookData(R.drawable.home_item_book1))
+        }
+    }
 }
+
---
+++ LIBROG/app/src/main/java/com/example/librog/ui/main/history/HistoryRVAdapter.kt
@@ -0,0 +1,52 @@
+package com.example.librog.ui.main.history
+
+import android.view.LayoutInflater
+import android.view.ViewGroup
+import androidx.recyclerview.widget.RecyclerView
+import com.example.librog.data.HistoryBookData
+import com.example.librog.databinding.ItemHistoryBookBinding
+
+
+class HistoryRVAdapter (private val historyBookList: ArrayList<HistoryBookData>) : RecyclerView.Adapter<HistoryRVAdapter.ViewHolder>(){
+
+    interface OnItemClickListener {
+        fun onItemClick(tempHistoryBookData: HistoryBookData)
+    }
+
+    private lateinit var mItemClickListener: OnItemClickListener
+    fun setMyItemClickListener(itemClickListener: OnItemClickListener) {
+        mItemClickListener = itemClickListener
+    }
+
+
+    override fun onCreateViewHolder(viewGroup: ViewGroup, viewType: Int): HistoryRVAdapter.ViewHolder {
+        //아이템뷰 객체 생성
+        val binding: ItemHistoryBookBinding = ItemHistoryBookBinding.inflate(LayoutInflater.from(viewGroup.context), viewGroup, false)
+
+        return ViewHolder(binding) //아이템뷰 객체를 재활용하도록 뷰 홀더에게 던져줌
+    }
+
+    override fun onBindViewHolder(holder: HistoryRVAdapter.ViewHolder, position: Int) {
+        holder.bind(historyBookList[position]) //position=indexid 받아온 뷰홀더에 바인딩을 해주기 위해 해당 포지션의 데이터를 던져줌
+        holder.binding.itemHistoryBookIv.setOnClickListener {
+            mItemClickListener.onItemClick(historyBookList[position])
+        }
+    }
+
+    override fun getItemCount(): Int = historyBookList.size
+
+    inner class ViewHolder(val binding: ItemHistoryBookBinding): RecyclerView.ViewHolder(binding.root){
+
+        fun bind(historybook: HistoryBookData){
+            binding.itemHistoryBookIv.setImageResource(historybook.coverImg!!)
+        }
+
+    }
+
+
+}
+
+
+
+
+
