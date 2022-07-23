f2e5d14eae59a0769b5e7cd139331569cddb2a7a
hyun2371 <loveanna1228@gmail.com>
2022-07-23 18:57:18 +0900

    Design: Add profile layout

--- LIBROG/app/src/main/java/com/example/librog/ui/main/mypage/MypageFragment.kt
+++ LIBROG/app/src/main/java/com/example/librog/ui/main/mypage/MypageFragment.kt
@@ -6,6 +6,7 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.appcompat.app.AppCompatActivity
 import com.example.librog.R
 import com.example.librog.data.HistoryBookData
 import com.example.librog.databinding.FragmentHistoryBinding
@@ -25,16 +26,41 @@ class MypageFragment : Fragment() {
     ): View? {
         binding = FragmentMypageBinding.inflate(inflater, container, false)
 
-        initClickListener()
+        initViews()
 
         return binding.root
     }
 
-    private fun initClickListener(){
-        binding.mypageLoginBtn.setOnClickListener {
-            val intent = Intent(activity, LoginActivity::class.java)
-            startActivity(intent)
+
+    private fun getJwt(): Int{
+        val spf = activity?.getSharedPreferences("auth",AppCompatActivity.MODE_PRIVATE) //fragment->?추가
+        return spf!!.getInt("jwt",0) //기본값 0
+    }
+
+    private fun initViews(){
+        val jwt : Int = getJwt()
+
+        if (jwt==0){ //기본값(로그인x)
+            binding.mypageLoginBtn.text = "로그인"
+            binding.mypageLoginBtn.setOnClickListener {
+                val intent = Intent(activity, LoginActivity::class.java)
+                startActivity(intent)
+            }
+        } else {
+            binding.mypageLoginBtn.text = "로그아웃"
+            binding.mypageLoginBtn.setOnClickListener {
+                val intent = Intent(activity, MainActivity::class.java)
+                logout()
+                startActivity(intent)
+            }
         }
+    }
+
+    private fun logout(){
+        val spf = activity?.getSharedPreferences("auth",AppCompatActivity.MODE_PRIVATE)
+        val editor = spf!!.edit()
+        editor.remove("jwt") //키값에 저장된값 삭제
+        editor.apply()
     }
 
 }
