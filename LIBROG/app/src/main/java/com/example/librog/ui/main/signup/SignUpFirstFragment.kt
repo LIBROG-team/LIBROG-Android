c7053e6dfb09146af4ee21b27456cb454976d422
hyun2371 <loveanna1228@gmail.com>
2022-08-11 14:14:58 +0900

    Feat: Signup validation check

--- LIBROG/app/src/main/java/com/example/librog/ui/main/signup/SignUpFirstFragment.kt
+++ LIBROG/app/src/main/java/com/example/librog/ui/main/signup/SignUpFirstFragment.kt
@@ -1,12 +1,15 @@
 package com.example.librog.ui.main.signup
 
 import android.util.Log
+import android.util.Patterns
+import android.view.View
 import com.example.librog.R
 import com.example.librog.data.remote.data.auth.AuthService
 import com.example.librog.data.remote.data.auth.SignUpInfo
 import com.example.librog.data.remote.data.auth.SignUpView
 import com.example.librog.databinding.FragmentSignupFirstBinding
 import com.example.librog.ui.BaseFragment
+import java.util.regex.Pattern
 
 class SignUpFirstFragment : BaseFragment<FragmentSignupFirstBinding>(FragmentSignupFirstBinding::inflate),SignUpView {
     override fun initAfterBinding() {
@@ -25,10 +28,11 @@ class SignUpFirstFragment : BaseFragment<FragmentSignupFirstBinding>(FragmentSig
     }
 
     private fun signUp(){
-        val authService = AuthService()
-        authService.setSignUpView(this)
-        Log.d("SIGNUP/",getSignUpInfo().toString())
-        authService.signUp(getSignUpInfo()) //api호출
+        validationCheck()
+//        val authService = AuthService()
+//        authService.setSignUpView(this)
+//        Log.d("SIGNUP/",getSignUpInfo().toString())
+//        authService.signUp(getSignUpInfo()) //api호출
     }
 
     override fun onSignUpSuccess(message: String) {
@@ -43,5 +47,43 @@ class SignUpFirstFragment : BaseFragment<FragmentSignupFirstBinding>(FragmentSig
         (context as SignUpActivity).supportFragmentManager.beginTransaction()
             .replace(R.id.sign_up_frame, SignUpLastFragment())
             .commitAllowingStateLoss()
+    }
+
+    private fun validationCheck(){
+        val pattern: Pattern = Patterns.EMAIL_ADDRESS
+        var error="0"
+        if (binding.suIdEt.text.toString().isEmpty()){
+            error="이메일을 입력해주세요"
+        }
+        else if (!pattern.matcher(binding.suIdEt.text.toString()).matches()){
+            error="이메일 형식을 정확하게 입력해주세요"
+        }
+        else if (binding.suIdEt.text.toString().length>30){
+            error="이메일은 30자리 미만으로 입력해주세요"
+        }
+        else if (binding.suPwdEt.text.toString().isEmpty()){
+            error="비밀번호를 입력해주세요"
+        }
+        else if (binding.suPwdEt.text.toString().length>21 || binding.suPwdEt.text.toString().length<7){
+            error = "비밀번호는 8~20자리를 입력해주세요"
+        }
+        else if (binding.suNameEt.text.toString().isEmpty()){
+            error="이름을 입력해주세요"
+        }
+        else if (binding.suNameEt.text.toString().length>21){
+            error="이름은 20자보다 짧게 입력해주세요"
+        }
+        else{
+            changeFragment()
+        }
+
+        showError(error)
+
+
+    }
+
+    private fun showError(error:String){
+        binding.suErrorTv.visibility = View.VISIBLE
+        binding.suErrorTv.text = error
     }
 }
