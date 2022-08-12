1074d9541ff88c3c1db3701bdbdbd7df0f370a1a
Lisiant <wkdxod321@gmail.com>
2022-08-12 16:36:49 +0900

    Feat: add DetailHistoryActivity - 독서기록 상세페이지 화면 작성 완료

---
+++ LIBROG/app/src/main/java/com/example/librog/ui/main/history/DetailHistoryActivity.kt
@@ -0,0 +1,11 @@
+package com.example.librog.ui.main.history
+
+
+import com.example.librog.databinding.ActivityDetailHistoryBinding
+import com.example.librog.ui.BaseActivity
+
+class DetailHistoryActivity : BaseActivity<ActivityDetailHistoryBinding>(ActivityDetailHistoryBinding::inflate) {
+
+    override fun initAfterBinding() {
+    }
+}
